a25fd0b58a69f375ac96608411e469abf7857c82
caaiomf <caiofavarao@hotmail.com>
2025-11-11 13:02:17 -0300

    Inclusao de alguns menus de login e logout, e tambem validacao apartir do postman

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ server.use(express.urlencoded({extended: true}));
 //querystring se for false
 
 server.get("/", (requisicao, resposta) =>{
-    //menu
-    resposta.send(`
+    //menu 
+    resposta.send(`  
         <!doctype html>
         <html lang="pt-br">
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Bootstrap demo</title>
+            <title>Menu</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         </head>
         <body>
@@ -41,7 +41,7 @@ server.get("/", (requisicao, resposta) =>{
                         </a>
                         <ul class="dropdown-menu">
                             <li class="nav-item"><a class="nav-link active" href="/cadastroFornecedor">Cadastro Fornecedor</a></li>
-                            <li class="nav-item"><a class="nav-link active" href="/listarFornecedores">Listar Fornecedores</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="/listaFornecedores">Listar Fornecedores</a></li>
                         </ul>
                         <li class="nav-item">
                         <a class="nav-link active" aria-current="/" href="/logout">Sair</a>
@@ -50,6 +50,25 @@ server.get("/", (requisicao, resposta) =>{
                     </div>
                 </div>
                 </nav>
+                <div class="container">
+                <h1 class="text-center border m-3 p-3 bg-light">Login</h1>
+
+                <form method="POST" action="/cadastroFornecedor" class="m-3 p-4 bg-light rounded shadow-sm col-md-6 mx-auto">
+                    <div class="mb-3">
+                        <label for="usuario" class="form-label">Usuário</label>
+                        <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Digite seu usuário">
+                        </div>
+                    <div class="mb-3">
+                        <label for="senha" class="form-label">Senha</label>
+                        <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite sua senha">
+                    </div>
+
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">Entrar</button>
+                        <a href="/" class="btn btn-secondary">Voltar</a>
+                    </div>
+                </form>
+            </div>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
             </body>
             </html>
@@ -67,6 +86,32 @@ server.get("/cadastroFornecedor", (requisicao,resposta) =>{
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         </head>
         <body>
+                        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">Menu</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cadastro
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link active" href="/cadastroFornecedor">Cadastro Fornecedor</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="/listaFornecedores">Listar Fornecedores</a></li>
+                        </ul>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="/" href="/logout">Sair</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
             <div class="container">
                 <h1 class="text-center border m-3 p-3 bg-light">Cadastro de Fornecedor </h1>
                 <form method="POST" action="/adicionarFornecedor" class="row g-3 needs-validation m-3 p-3 bg-light">
@@ -196,101 +241,100 @@ else{
                 <form method="POST" action="/adicionarFornecedor" class="row g-3 needs-validation m-3 p-3 bg-light">
                 <div class="col-md-4">
                     <label for="nomeFantasia" class="form-label">Nome Fantasia</label>
-                    <input type="text" class="form-control" id="nomeFantasia" name="nomeFantasia" value="">
-                </div>`;
+                    <input type="text" class="form-control" id="nomeFantasia" name="nomeFantasia" value=${nomeFantasia}>
+                `;
             if(!nomeFantasia){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o nome fantasia </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                 <div class="col-md-4">
                     <label for="razaoSocial" class="form-label">Razão Social</label>
-                    <input type="text" class="form-control" id="razaoSocial" name="razaoSocial" value="">
-                </div>`;
+                    <input type="text" class="form-control" id="razaoSocial" name="razaoSocial" value=${razaoSocial}>
+                `;
             if(!razaoSocial){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe a Razão Social </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                   <div class="col-md-4">
                       <label for="CNPJ" class="form-label">CNPJ</label>
-                      <input type="text" class="form-control" id="CNPJ" name="CNPJ" placeholder="00.000.000/0000-00" maxlength="14">
-                  </div>`;
+                      <input type="text" class="form-control" id="CNPJ" name="CNPJ" placeholder="00.000.000/0000-00" maxlength="18" value=${CNPJ}>
+                  `;
             if(!CNPJ){
             conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o CNPJ </p>
                 </div>`;
             };
-            conteudo +=`      
+            conteudo +=`</div>      
                 <div class="row g-3">
                     <div class="col-md-3">
                         <label for="CEP" class="form-label">CEP</label>
-                        <input type="text" id="CEP" name="CEP"  class="form-control" placeholder="00000-000" maxlength="8">
-                    </div>`;
+                        <input type="text" id="CEP" name="CEP"  class="form-control" placeholder="00000-000" maxlength="9" value=${CEP}>
+                    `;
             if(!CEP){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o CEP do fornecedor </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                     <div class="col-md-5">
                         <label for="logradouro" class="form-label">Logradouro</label>
-                        <input type="text" id="logradouro" name="logradouro" class="form-control">
-                    </div>`;
+                        <input type="text" id="logradouro" name="logradouro" class="form-control" value=${logradouro}>
+                    `;
             if(!logradouro){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o Logradouro do fornecedor </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                     <div class="col-md-2">
                         <label for="numero" class="form-label">Número</label>
-                        <input type="text" id="numero" name="numero" class="form-control">
-                    </div>`;
+                        <input type="number" id="numero" name="numero" class="form-control" value=${numero}>
+                    `;
             if(!numero){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o Número </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                     <div class="col-md-2">
                         <label for="complemento" class="form-label">Complemento</label>
-                        <input type="text" id="complemento" name="complemento"  class="form-control">
+                        <input type="text" id="complemento" name="complemento"  class="form-control" value=${complemento}>
                     </div>
                     <div class="col-md-4">
                         <label for="bairro" class="form-label">Bairro</label>
-                        <input type="text" id="bairro" name="bairro"  class="form-control">
-                    </div>`;
+                        <input type="text" id="bairro" name="bairro"  class="form-control" value=${bairro}>
+                    `;
             if(!bairro){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o Bairro do fornecedor </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                     <div class="col-md-4">
                         <label for="cidade" class="form-label">Cidade</label>
-                        <input type="text" id="cidade" name="cidade"  class="form-control">
-                    </div>`;
+                        <input type="text" id="cidade" name="cidade"  class="form-control" value=${cidade}>
+                    `;
             if(!cidade){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe a cidade do fornecedor </p>
                 </div>`;
             };
-            conteudo +=`
-
+            conteudo +=`</div>
                     <div class="col-md-4">
                         <label for="UF" class="form-label">Estado (UF)</label>
-                            <select class="form-select" id="UF" name="UF">
+                            <select class="form-select" id="UF" name="UF" value=${UF}>
                                 <option value="">Selecione o estado...</option>
                                 <option value="AC">Acre (AC)</option>
                                 <option value="AL">Alagoas (AL)</option>
@@ -320,36 +364,36 @@ else{
                                 <option value="SE">Sergipe (SE)</option>
                                 <option value="TO">Tocantins (TO)</option>
                             </select>
-                    </div>`;
+                    `;
             if(!UF){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o estado do fornecedor </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                     <div class="col-md-4">
                         <label for="email" class="form-label">E-mail</label>
-                        <input type="text" id="email" name="email"  class="form-control">
-                    </div>`;
+                        <input type="text" id="email" name="email"  class="form-control" value=${email}>
+                    `;
             if(!email){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o E-mail do fornecedor </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                     <div class="col-md-4">
                         <label for="telefone" class="form-label">Telefone</label>
-                        <input type="text" id="telefone" name="telefone"  class="form-control">
-                    </div>`;
+                        <input type="text" id="telefone" name="telefone"  class="form-control" value=${telefone}>
+                    `;
             if(!telefone){
                 conteudo += `
                 <div>
                     <p class="text-danger">Porfavor, informe o Telefone do fornecedor </p>
                 </div>`;
             };
-            conteudo +=`
+            conteudo +=`</div>
                 </div>
                 <div class="col-12">
                     <br><button class="btn btn-primary" type="submit">Cadastrar</button>
@@ -362,8 +406,7 @@ else{
         </body>
 </html>
 `;
-
-    resposta.send(conteudo)
+        resposta.send(conteudo);
 }
 });
 
@@ -378,6 +421,32 @@ server.get("/listaFornecedores", (requisicao, resposta) => {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         </head>
         <body>
+                        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">Menu</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cadastro
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link active" href="/cadastroFornecedor">Cadastro Fornecedor</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="/listaFornecedores">Listar Fornecedores</a></li>
+                        </ul>
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="/" href="/logout">Sair</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
             <div class="container mt-5">
             <h2 class="text-center mb-4">Lista de Fornecedores Cadastrados</h2>
             
@@ -388,8 +457,6 @@ server.get("/listaFornecedores", (requisicao, resposta) => {
                         <th>Razão Social</th>
                         <th>Nome Fantasia</th>
                         <th>CNPJ</th>
-                        <th>Inscricao Municipal</th>
-                        <th>Seguimento</th>
                         <th>CEP</th>
                         <th>Logradouro</th>
                         <th>Numero</th>
@@ -397,6 +464,8 @@ server.get("/listaFornecedores", (requisicao, resposta) => {
                         <th>Bairro</th>
                         <th>Cidade</th>
                         <th>Estado</th>
+                        <th>E-mail</th>
+                        <th>Telefone</th>
                     </tr>
                 </thead>
                 <tbody id="tabela-fornecedores">`;
@@ -406,8 +475,6 @@ server.get("/listaFornecedores", (requisicao, resposta) => {
                     <td>${listaFornecedor[i].razaoSocial}</td>
                     <td>${listaFornecedor[i].nomeFantasia}</td>
                     <td>${listaFornecedor[i].CNPJ}</td>
-                    <td>${listaFornecedor[i].inscricao}</td>
-                    <td>${listaFornecedor[i].segmento}</td>
                     <td>${listaFornecedor[i].CEP}</td>
                     <td>${listaFornecedor[i].logradouro}</td>
                     <td>${listaFornecedor[i].numero}</td>
@@ -415,6 +482,8 @@ server.get("/listaFornecedores", (requisicao, resposta) => {
                     <td>${listaFornecedor[i].bairro}</td>
                     <td>${listaFornecedor[i].cidade}</td>
                     <td>${listaFornecedor[i].UF}</td>
+                    <td>${listaFornecedor[i].email}</td>
+                    <td>${listaFornecedor[i].telefone}</td>
                 </tr>
             `;
         }
@@ -429,6 +498,33 @@ server.get("/listaFornecedores", (requisicao, resposta) => {
         </html>
         `;
                 resposta.send(conteudo);
+});
+server.get("/logout", (requisicao,resposta) =>{ 
+    resposta.send(`
+        <!doctype html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Logout</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        </head>
+        <body>
+            <div class="container">
+                <h1 class="text-center border m-3 p-3 bg-light">Logout</h1>
+
+                <div class="row justify-content-center m-3 p-3 bg-light rounded shadow-sm">
+                    <div class="col-md-8 text-center">
+                        <p class="fs-5">Obrigado pela visita</p>
+                            <a href="/" class="btn btn-danger m-2">Voltar</a>
+                    </div>
+                </div>
+            </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        </body>
+        </html>
+`);
 });
 server.listen(porta, host, ()=>{
     console.log (`servidor rodando em http://${host}:${porta}`)
